11971c349d8fcc9db137631e6334285225e3ded6
Ethan Huang <42885936+ethanhuang03@users.noreply.github.com>
2025-02-01 00:15:40 -0500

    created config file to unify variables, still WIP

--- CONFIG.py
+++ CONFIG.py
@@ -1,31 +1,21 @@
 '''
 A file to store all the USEFUL variables that span multiple files.
-
-TODO: Currently, it is just a list of variables used. Actually make it syntactically correct.
 '''
 # APP : search.py
-ES = Elasticsearch("http://localhost:9200/")
-SBERT_MODEL = SentenceTransformer("all-MiniLM-L6-v2")
-INDEX = "nls_search_final"
+# PROCESSOR : processor.py
+# PROCESSOR : verify_index.py
+ELASTICSEARCH_HOST = "http://localhost:9200/"
+SBERT_MODEL_NAME = "all-MiniLM-L6-v2"
+INDEX_NAME = "nls_search_final"
 
 # PROCESSOR : processor.py
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Yixio\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-ES = Elasticsearch("http://localhost:9200/")
-SBERT_MODEL = SentenceTransformer("all-MiniLM-L6-v2")
-INDEX = "nls_search_final"
-folder_path = input("Please enter the folder path: ")
-
-# PROCESSOR : verify_index.py
-ES = Elasticsearch("http://localhost:9200/")
-INDEX = "nls_search_final"
+PYTESSERACT_PATH = r'C:\Users\Yixio\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+PROCESSORED_FOLDER_PATH = ""  # Is this the downloaded_file dir from the scraper?
 
 # SCRAPER/CRAWLER : settings.py
-NDJSON_FILE_NAME = "crawled_links/visited_urls.json"
-
-# SCRAPER/DOWNLOADER : settings.py
-ndjson_file = "./crawled_links/visited_urls.json"
-download_dir = "downloader/files"  # Change this to an absolute path outside. 
-checkpoint_file = "downloader/files/checkpoint.txt"
-
+# SCRAPER/DOWNLOADER : main.py
 # SCRAPER/VISUALISE : visualiser.py
-NDJSON_FILE = "./crawled_links/visited_urls.json"  # Path to your NDJSON file
+VISITED_URLS_FILE = "crawled_links/visited_urls.json"
+DOWNLOADED_FILES_DIR = "downloader/files"
+CHECKPOINT_FILE_NAME = "downloader/files/checkpoint.txt"
+
